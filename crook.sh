a5d94279b827d60b7f703ebf1e6e75161777face
Piyush Ahuja <piyushahuja.in@gmail.com>
2020-08-25 15:06:48 +0100

    Dummy script to parse log file

--- crook.sh
+++ crook.sh
@@ -12,17 +12,20 @@
 
 # Steps:
 
-# 1. Assign fd 123 as input to a file named ".lock"
-# 2. Obtain a write lock on file descriptor 123.
-# 3. Saved lock status in variable locked
+# 1. Assign fd 123 as input to a file named ".lock" 2. Obtain a write lock on file descriptor 123.  3. Saved lock status in variable locked
+# Open connection to the SQLite database to track Shepherd Job IDs
+
 # 4. If ready option is passed:
-     # exit with the status of locked (If locked successfully, exit with 0, else with 1
      # shepherd ready && exit 1 (Exit Code 1: Shepherd is busy with a previous job.)
      # check shephord capacity (Exit Code 2: The requested capacity is not available.
-     # submit_to_shepherd -0  "name formating..." --remove-files 
+       # exit with the status of locked (If locked successfully, exit with 0, else with 1
+    
+    # When a "ready challenge" is received, crook must:
+        # Update the status of all incomplete Shepherd jobs in its tracking database.
+    # Return positively if all jobs have completed; otherwise return negatively.
 
 # 5. If ready option is not passed, then if locked successfully do nothing. If locked unsuccessfully, exit with 1. 
-# 6. xargs 
+# 6. xargs  # submit_to_shepherd -0  "name formating..." --remove-files 
 
 
 exec 123>.lock
@@ -38,20 +41,24 @@ locked=$?
 
 
 case $1 in
-  ready)  exit ${locked};;
-     # shepherd ready && exit 1 (Exit Code 1: Shepherd is busy with a previous job.)
-     # check shephord capacity (Exit Code 2: The requested capacity is not available.)
-
-
+  ready)  
+     # shepherd ready && exit 1;; # (Exit Code 1: Shepherd is busy with a previous job.)
+     # no_capacity=check_shephord_capacity $2;; #(Exit Code 2: The requested capacity is not available.)
+     # no_capacity && exit 2;;
+     exit ${locked};;
   *)      (( locked )) && exit 1;;
 esac
 
 
-xargs -0 tar czf "/archive/$(date +%F).tar.gz" --remove-files
+xargs tar czf "$(date +%F).tar.gz"
+
+# xargs -0 submit_to_shepherd "name formating..." --remove-files (Exit Code 0: The input data was fully consumed and passed to Shepherd successfully.)
     # Processing of the stream could not be concluded before handling to Shepherd.
-    # submit_to_shepherd -0  "name formating..." --remove-files (Exit Code 0: The input data was fully consumed and passed to Shepherd successfully.)
+    
 
 
 # crook must only exit with a successful exit code if the process was fully successful, as this will instruct HGI Vault to irrecoverably clear its staging queue. Ensure that any unexpected exit will result in a failure exit code.
+
+# crook will only ever be run serially, by a single user, so its tracking database can be file-based (e.g., SQLite). Shepherd maintains the files it needs to process internally, so these will not also need to be maintained by crook.
 
 
